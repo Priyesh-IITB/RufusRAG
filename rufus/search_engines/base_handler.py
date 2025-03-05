e3679593555057d1a223ca022c57f6d755b781f3
Priyesh Verma <156275728+Priyesh-IITB@users.noreply.github.com>
2025-03-05 00:47:27 -0600

    Update base_handler.py

--- rufus/search_engines/base_handler.py
+++ rufus/search_engines/base_handler.py
@@ -1,1 +1,7 @@
+from abc import ABC, abstractmethod
 
+class SearchEngineHandler(ABC):
+    @abstractmethod
+    def get_search_results(self, query, **kwargs):
+        """Search the search engine for the given query."""
+        pass
