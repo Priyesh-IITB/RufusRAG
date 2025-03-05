38b664b5439d81809dcf93bc32b8c74017af0a5a
Priyesh Verma <156275728+Priyesh-IITB@users.noreply.github.com>
2025-03-05 00:49:00 -0600

    Update test_generate_search_query.py

--- tests/test_generate_search_query.py
+++ tests/test_generate_search_query.py
@@ -1,1 +1,13 @@
+from rufus.llms.method import generate_search_query
 
+def test_generate_search_query_with_google():
+    config = {
+        "llm_provider": "google",
+        "llm_api_key": "AIzaSyAfn-WV-ZhcpT-GwpRY6J-FyQcMn5zlT14",
+        "llm_name": "models/gemini-1.5-flash-latest"
+    }
+    prompt = "Find information about product features."
+    url = "https://apple.com"
+    query = generate_search_query(prompt, url, **config)
+    assert isinstance(query, str)
+    assert len(query) > 0
