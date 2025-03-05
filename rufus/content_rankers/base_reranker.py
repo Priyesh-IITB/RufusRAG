fe55fb6f3c285acb28239ff1ffdbced110b697c2
Priyesh Verma <156275728+Priyesh-IITB@users.noreply.github.com>
2025-03-05 00:43:49 -0600

    Update base_reranker.py

--- rufus/content_rankers/base_reranker.py
+++ rufus/content_rankers/base_reranker.py
@@ -1,1 +1,7 @@
+from abc import ABC, abstractmethod
 
+class BaseReranker(ABC):
+    @abstractmethod
+    def get_embeddings(self, texts):
+        """Obtain embeddings for the given list of texts."""
+        pass
