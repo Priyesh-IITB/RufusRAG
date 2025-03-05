564d4ad7e4478fca624128988aec66ae687e14bf
Priyesh Verma <156275728+Priyesh-IITB@users.noreply.github.com>
2025-03-05 00:44:16 -0600

    Update google_text_embedding_reranker.py

--- rufus/content_rankers/google_text_embedding_reranker.py
+++ rufus/content_rankers/google_text_embedding_reranker.py
@@ -1,1 +1,15 @@
+import google.generativeai as genai
+from .base_reranker import BaseReranker
 
+class GoogleTextEmbeddingReranker(BaseReranker):
+    def __init__(self, embd_model_api_key, embd_model_name):
+        self.model_name = embd_model_name
+        genai.configure(api_key=embd_model_api_key)
+    
+    def get_embeddings(self, texts):
+        try:
+            response = genai.embed_content(model=self.model_name, content=texts)
+            return response['embedding']
+        except Exception as e:
+            print(f"Error fetching embeddings: {e}")
+            return []
